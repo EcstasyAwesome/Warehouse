817a92b223e900eb776bbca64c20207a42c3368e
EcstasyAwesome <ecstasy.awesome@gmail.com>
2021-04-20 12:45:33 +0300

    add Language enum, minor fixes

--- src/main/java/com/github/ecstasyawesome/warehouse/util/ResourceLoader.java
+++ src/main/java/com/github/ecstasyawesome/warehouse/util/ResourceLoader.java
@@ -1,6 +1,9 @@
 package com.github.ecstasyawesome.warehouse.util;
 
+import com.github.ecstasyawesome.warehouse.core.ApplicationSettings;
+import java.io.IOException;
 import java.net.URL;
+import java.util.Locale;
 import java.util.Objects;
 import java.util.ResourceBundle;
 import javafx.fxml.FXMLLoader;
@@ -8,7 +11,7 @@ import javafx.scene.Parent;
 
 public final class ResourceLoader {
 
-  public static final ResourceBundle LANGUAGE_BUNDLE = loadResourceBundle();
+  private static final ResourceBundle LANGUAGE_BUNDLE = loadResourceBundle();
 
   private ResourceLoader() {
   }
@@ -17,13 +20,34 @@ public final class ResourceLoader {
     Objects.requireNonNull(resource);
     try {
       return FXMLLoader.load(resource, LANGUAGE_BUNDLE);
-    } catch (Exception exception) {
-      throw new ExceptionInInitializerError(exception);
+    } catch (IOException exception) {
+      // TODO save to some logger
+      throw new IllegalArgumentException(exception);
     }
   }
 
+  public static FXMLLoader createFxmlLoader(URL resource) {
+    Objects.requireNonNull(resource);
+    return new FXMLLoader(resource, LANGUAGE_BUNDLE);
+  }
+
   private static ResourceBundle loadResourceBundle() {
-    // TODO preload locale from app settings
-    return ResourceBundle.getBundle("language/locale");
+    var settings = ApplicationSettings.getInstance();
+    return ResourceBundle.getBundle("language/locale", settings.getLanguage().locale);
+  }
+
+  public enum Language {
+
+    ENGLISH(Locale.ROOT, "English"),
+    RUSSIAN(new Locale("ru", "RU"), "Русский"),
+    UKRAINIAN(new Locale("uk", "UA"), "Українська");
+
+    private final Locale locale;
+    public final String originalName;
+
+    Language(Locale locale, String originalName) {
+      this.locale = locale;
+      this.originalName = originalName;
+    }
   }
 }
