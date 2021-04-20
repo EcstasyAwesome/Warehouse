8f317aa16537e29bcfc10d4854c99446339231ed
EcstasyAwesome <ecstasy.awesome@gmail.com>
2021-04-20 11:01:08 +0300

    rebuild class, now alerts are here

--- src/main/java/com/github/ecstasyawesome/warehouse/core/WindowManager.java
+++ src/main/java/com/github/ecstasyawesome/warehouse/core/WindowManager.java
@@ -1,8 +1,9 @@
 package com.github.ecstasyawesome.warehouse.core;
 
-import com.github.ecstasyawesome.warehouse.module.controller.Authorization;
-import com.github.ecstasyawesome.warehouse.service.EventManager;
-import com.github.ecstasyawesome.warehouse.util.ResourceLoader;
+import com.github.ecstasyawesome.warehouse.model.User;
+import com.github.ecstasyawesome.warehouse.module.AuthorizationModuleFactory;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -10,7 +11,12 @@ import javafx.animation.FadeTransition;
 import javafx.event.EventHandler;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
@@ -23,12 +29,9 @@ public final class WindowManager {
   private final Stage authorizationStage = new Stage();
   private final Stage mainStage = new Stage();
   private final List<Stage> stages = new ArrayList<>();
-  private final ViewSettings viewSettings = ViewSettings.getInstance();
 
   private WindowManager(final Stage root) {
     this.root = root;
-    configureMainStage();
-    configureAuthorizationStage();
   }
 
   public static WindowManager getInstance() {
@@ -45,21 +48,28 @@ public final class WindowManager {
     instance = new WindowManager(stage);
   }
 
-  public void showAuthorizationForm() {
-    if (Authorization.getCurrentUser().isEmpty()) {
+  public void showAuthorization() {
+    if (authorizationStage.getOwner() == null) {
+      configureAuthorizationStage();
+    }
+    var user = getUserFromContext();
+    if (user.isEmpty()) {
       mainStage.close();
       authorizationStage.show();
     } else {
-      EventManager.showPopUpWindow(AlertType.WARNING, "Do logout before login"); // TODO i18n
+      showDialog(AlertType.WARNING, "Do logout before login"); // TODO i18n
     }
   }
 
   public <T extends Controller> void show(ModuleFactory<T> factory) {
-    var wrapper = factory.create();
-    if (Authorization.isAccessGranted(wrapper.getAccess())) {
+    if (mainStage.getOwner() == null) {
+      configureMainStage();
+    }
+    var module = factory.create();
+    if (isAccessGranted(module.getAccess())) {
       authorizationStage.close();
       closeSecondaryStagesIfPresent();
-      mainStage.setScene(wrapper.getScene());
+      mainStage.setScene(module.getScene());
       mainStage.show();
     } else {
       showAccessWarning();
@@ -68,15 +78,76 @@ public final class WindowManager {
 
   public <T extends FeedbackController<E>, E> Optional<E> show(
       FeedbackModuleFactory<T, E> factory) {
-    var wrapper = factory.create();
-    if (Authorization.isAccessGranted(wrapper.getAccess())) {
-      var stage = createNewStage(wrapper.getScene());
+    var module = factory.create();
+    if (isAccessGranted(module.getAccess())) {
+      var stage = createNewStage(module.getScene());
       stages.add(stage);
       stage.showAndWait();
+      return Optional.ofNullable(module.getController().take());
     } else {
       showAccessWarning();
     }
-    return Optional.ofNullable(wrapper.getController().take());
+    return Optional.empty();
+  }
+
+  // TODO push notifications (will be super to find lib to show native notifications)
+
+  public Optional<ButtonType> showDialog(AlertType type, final String message) {
+    return createNewAlertDialog(type, message).showAndWait();
+  }
+
+  public void showDialog(final Exception exception) {
+    var alert = createNewAlertDialog(AlertType.ERROR, exception.getMessage());
+    var stacktrace = getStacktrace(exception);
+
+    var textArea = new TextArea(stacktrace);
+    textArea.setEditable(false);
+    textArea.setWrapText(true);
+    textArea.setMaxWidth(Double.MAX_VALUE);
+    textArea.setMaxHeight(Double.MAX_VALUE);
+    GridPane.setVgrow(textArea, Priority.ALWAYS);
+    GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+    var gridPane = new GridPane();
+    gridPane.setMaxWidth(Double.MAX_VALUE);
+    gridPane.add(textArea, 0, 0);
+
+    alert.getDialogPane().setExpandableContent(gridPane);
+    alert.showAndWait();
+  }
+
+  private Alert createNewAlertDialog(AlertType type, String message) {
+    var alert = new Alert(type);
+    alert.setTitle("Title"); // TODO i18n
+    alert.setHeaderText(null);
+    var defaultMessage = "Message is not provided"; // TODO i18n
+    var messageNullOrBlank = message == null || message.isBlank();
+    alert.setContentText(messageNullOrBlank ? defaultMessage : message);
+    return alert;
+  }
+
+  private String getStacktrace(Exception exception) {
+    var result = new StringWriter();
+    try (var printStream = new PrintWriter(result)) {
+      exception.printStackTrace(printStream);
+    }
+    return result.toString();
+  }
+
+  private boolean isAccessGranted(Access access) {
+    var user = getUserFromContext();
+    if (access == Access.GUEST) {
+      return true;
+    }
+    if (user.isEmpty()) {
+      return false;
+    }
+    return user.get().getAccess().level <= access.level;
+  }
+
+  private Optional<User> getUserFromContext() {
+    var user = (User) SessionManager.get("currentUser").orElse(null);
+    return Optional.ofNullable(user);
   }
 
   private void closeSecondaryStagesIfPresent() {
@@ -86,7 +157,7 @@ public final class WindowManager {
   }
 
   private void showAccessWarning() {
-    EventManager.showPopUpWindow(AlertType.WARNING, "Access denied!"); // TODO i18n
+    showDialog(AlertType.WARNING, "Access denied!"); // TODO i18n
   }
 
   private Stage createNewStage(Scene scene) {
@@ -101,6 +172,8 @@ public final class WindowManager {
   }
 
   private void configureMainStage() {
+    var viewSettings = ViewSettings.getInstance();
+    mainStage.setTitle("Warehouse"); // TODO i18n
     mainStage.initOwner(root);
     mainStage.setMinWidth(viewSettings.getDefaultWidth());
     mainStage.setMinHeight(viewSettings.getDefaultHeight());
@@ -111,10 +184,11 @@ public final class WindowManager {
   }
 
   private void configureAuthorizationStage() {
-    var authorization = ResourceLoader.load(Authorization.FXML);
-    applyFadeAnimation(authorization);
+    var module = AuthorizationModuleFactory.INSTANCE.create();
+    applyFadeAnimation(module.getParent());
+    authorizationStage.setTitle("Warehouse login"); // TODO i18n
     authorizationStage.initOwner(root);
-    authorizationStage.setScene(new Scene(authorization));
+    authorizationStage.setScene(module.getScene());
     authorizationStage.setResizable(false);
   }
 
@@ -128,6 +202,7 @@ public final class WindowManager {
 
   private EventHandler<WindowEvent> getOnCloseActions() {
     return event -> {
+      var viewSettings = ViewSettings.getInstance();
       viewSettings.setWidth(mainStage.getWidth());
       viewSettings.setHeight(mainStage.getHeight());
       viewSettings.setMaximized(mainStage.isMaximized());
