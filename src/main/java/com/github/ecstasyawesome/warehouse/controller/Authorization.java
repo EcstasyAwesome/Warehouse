97cde36119554220e867f78662de4641b7dd52e3
EcstasyAwesome <ecstasy.awesome@gmail.com>
2021-04-22 18:11:29 +0300

    complete an authorization part

--- src/main/java/com/github/ecstasyawesome/warehouse/controller/Authorization.java
+++ src/main/java/com/github/ecstasyawesome/warehouse/controller/Authorization.java
@@ -2,7 +2,12 @@ package com.github.ecstasyawesome.warehouse.controller;
 
 import com.github.ecstasyawesome.warehouse.core.Controller;
 import com.github.ecstasyawesome.warehouse.core.WindowManager;
+import com.github.ecstasyawesome.warehouse.dao.UserDaoService;
+import com.github.ecstasyawesome.warehouse.model.User;
+import com.github.ecstasyawesome.warehouse.module.AdministratorRegistrationModuleFactory;
 import com.github.ecstasyawesome.warehouse.module.HomeModuleFactory;
+import com.github.ecstasyawesome.warehouse.util.SessionManager;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -11,6 +16,7 @@ import javafx.scene.effect.ColorAdjust;
 public class Authorization extends Controller {
 
   private final WindowManager windowManager = WindowManager.getInstance();
+  private final UserDaoService userDaoService = UserDaoService.INSTANCE;
 
   @FXML
   private TextField loginField;
@@ -21,16 +27,34 @@ public class Authorization extends Controller {
   @FXML
   void login() {
     var colorEffect = new ColorAdjust(0, 0.1, 0, 0);
-    if (loginField.getText().equals("")) { // TODO local user and DB users
-      loginField.setEffect(null);
-      if (passwordField.getText().equals("")) { // TODO local user and DB users
+    var user = (User) null;
+    try {
+      user = userDaoService.get(loginField.getText());
+    } catch (NullPointerException exception) {
+      loginField.setEffect(colorEffect);
+    } catch (Exception exception) {
+      windowManager.showDialog(exception);
+    }
+    if (user != null) {
+      if (passwordField.getText().equals(user.getPassword())) {
         passwordField.setEffect(null);
+        SessionManager.store("currentUser", user);
         windowManager.show(HomeModuleFactory.INSTANCE);
       } else {
         passwordField.setEffect(colorEffect);
       }
-    } else {
-      loginField.setEffect(colorEffect);
+    }
+  }
+
+  @FXML
+  private void initialize() {
+    if (userDaoService.isEmptyTable()) {
+      var result = windowManager.show(AdministratorRegistrationModuleFactory.INSTANCE);
+      if (result.isPresent()) {
+        loginField.setText(result.get());
+      } else {
+        Platform.exit();
+      }
     }
   }
 }
