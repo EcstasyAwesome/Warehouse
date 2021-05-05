6abbdbe113a05e592427778843910462703aa637
EcstasyAwesome <ecstasy.awesome@gmail.com>
2021-05-05 14:15:58 +0300

    update -move to non-modular -add dev info -update a few dependencies

---
+++ src/main/java/com/github/ecstasyawesome/warehouse/App.java
@@ -0,0 +1,11 @@
+package com.github.ecstasyawesome.warehouse;
+
+import com.github.ecstasyawesome.warehouse.core.Warehouse;
+import javafx.application.Application;
+
+public class App {
+
+  public static void main(String[] args) {
+    Application.launch(Warehouse.class, args);
+  }
+}
--- src/main/java/com/github/ecstasyawesome/warehouse/core/Warehouse.java
+++ src/main/java/com/github/ecstasyawesome/warehouse/core/Warehouse.java
@@ -1,14 +1,9 @@
-package com.github.ecstasyawesome.warehouse;
+package com.github.ecstasyawesome.warehouse.core;
 
-import com.github.ecstasyawesome.warehouse.core.WindowManager;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Warehouse extends Application {
-
-  public static void main(String[] args) {
-    launch(args);
-  }
 
   @Override
   public void start(Stage primaryStage) {
