b9d0cd63138b1c85008811ecb59e1bb52e2d26be
EcstasyAwesome <ecstasy.awesome@gmail.com>
2021-05-11 18:34:41 +0300

    add new table, rename field of all tables, add table`s names (TODO)

--- src/main/java/com/github/ecstasyawesome/warehouse/util/ConnectionPool.java
+++ src/main/java/com/github/ecstasyawesome/warehouse/util/ConnectionPool.java
@@ -11,20 +11,43 @@ public final class ConnectionPool {
 
   private static final Logger LOGGER = LogManager.getLogger(ConnectionPool.class);
   private static final String USERS_TABLE = """
-      CREATE TABLE USERS (
-          ID          BIGINT PRIMARY KEY AUTO_INCREMENT,
-          SURNAME     VARCHAR(25) NOT NULL,
-          NAME        VARCHAR(25) NOT NULL,
-          SECOND_NAME VARCHAR(25) NOT NULL,
-          PHONE       VARCHAR(20) NOT NULL,
-          LOGIN       VARCHAR(20) NOT NULL UNIQUE,
-          PASSWORD    VARCHAR(20) NOT NULL,
-          ACCESS      VARCHAR(15) NOT NULL
+      CREATE TABLE USERS
+      (
+          USER_ID          BIGINT PRIMARY KEY AUTO_INCREMENT,
+          USER_SURNAME     VARCHAR(25) NOT NULL,
+          USER_NAME        VARCHAR(25) NOT NULL,
+          USER_SECOND_NAME VARCHAR(25) NOT NULL,
+          USER_PHONE       VARCHAR(20) NOT NULL,
+          USER_LOGIN       VARCHAR(20) NOT NULL UNIQUE,
+          USER_PASSWORD    VARCHAR(20) NOT NULL,
+          USER_ACCESS      VARCHAR(15) NOT NULL
+      )
+      """;
+  private static final String CATEGORIES_TABLE = """
+      CREATE TABLE CATEGORIES
+      (
+          CATEGORY_ID   BIGINT PRIMARY KEY AUTO_INCREMENT,
+          CATEGORY_NAME VARCHAR(30) NOT NULL UNIQUE
+      )
+      """;
+  private static final String PRODUCTS_TABLE = """
+      CREATE TABLE PRODUCTS
+      (
+          PRODUCT_ID   BIGINT PRIMARY KEY AUTO_INCREMENT,
+          PRODUCT_NAME VARCHAR(40) NOT NULL UNIQUE,
+          PRODUCT_UNIT VARCHAR(15) NOT NULL,
+          CATEGORY_ID  BIGINT      NOT NULL,
+          CONSTRAINT FK_CATEGORY_ID
+              FOREIGN KEY (CATEGORY_ID)
+                  REFERENCES CATEGORIES (CATEGORY_ID)
+                  ON DELETE CASCADE
       )
       """;
 
   static {
-    createTableIfExist(USERS_TABLE);
+    createTableIfExist(USERS_TABLE, "USERS");
+    createTableIfExist(CATEGORIES_TABLE, "CATEGORIES");
+    createTableIfExist(PRODUCTS_TABLE, "PRODUCTS");
   }
 
   public static Connection getConnection() throws SQLException {
@@ -32,16 +55,16 @@ public final class ConnectionPool {
     return DriverManager.getConnection("jdbc:h2:./database/default", "root", "root");
   }
 
-  private static void createTableIfExist(String sql) {
+  private static void createTableIfExist(String sql, String tableName) {
     try (var connection = getConnection()) {
       connection.setAutoCommit(false);
       try (var statement = connection.prepareStatement(sql)) {
         statement.execute();
         connection.commit();
-        LOGGER.debug("Table created"); // TODO table name
+        LOGGER.debug("Table created [{}]", tableName);
       } catch (SQLException exception) {
         connection.rollback();
-        LOGGER.debug("Table exists"); // TODO table name
+        LOGGER.debug("Table exists [{}]", tableName);
       }
     } catch (SQLException exception) {
       throw new ExceptionInInitializerError(LOGGER.throwing(Level.FATAL, exception));
