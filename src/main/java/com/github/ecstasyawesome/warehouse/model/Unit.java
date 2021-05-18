1573da5f7d79a334073c061d47ea2ed73c9c14d4
EcstasyAwesome <ecstasy.awesome@gmail.com>
2021-05-18 15:52:01 +0300

    add String<->Double converter with custom decimal formatter

--- src/main/java/com/github/ecstasyawesome/warehouse/model/Unit.java
+++ src/main/java/com/github/ecstasyawesome/warehouse/model/Unit.java
@@ -1,5 +1,23 @@
 package com.github.ecstasyawesome.warehouse.model;
 
+import java.text.DecimalFormat;
+import java.text.ParseException;
+
 public enum Unit {
-  PC, KILO, LITER
+
+  PC, KILO, LITER;
+
+  private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat(".00#");
+
+  public static String convert(Double value) {
+    return DECIMAL_FORMAT.format(value.doubleValue());
+  }
+
+  public static Double convert(String value) {
+    try {
+      return DECIMAL_FORMAT.parse(value).doubleValue();
+    } catch (ParseException e) {
+      return 0D;
+    }
+  }
 }
