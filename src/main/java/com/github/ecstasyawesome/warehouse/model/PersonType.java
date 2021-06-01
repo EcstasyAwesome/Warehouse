80c1d8f6792e793428622f1cd32ae4dff893701c
EcstasyAwesome <ecstasy.awesome@gmail.com>
2021-06-01 15:42:50 +0300

    improve PersonType -add method, that returns a observable list -add constructor -override toString

--- src/main/java/com/github/ecstasyawesome/warehouse/model/PersonType.java
+++ src/main/java/com/github/ecstasyawesome/warehouse/model/PersonType.java
@@ -1,5 +1,26 @@
 package com.github.ecstasyawesome.warehouse.model;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public enum PersonType {
-  INDIVIDUAL, LEGAL_ENTITY
+
+  // TODO i18n
+  INDIVIDUAL("Individual"),
+  LEGAL_ENTITY("Legal entity");
+
+  public final String name;
+
+  PersonType(String name) {
+    this.name = name;
+  }
+
+  public static ObservableList<PersonType> getPersonTypes() {
+    return FXCollections.observableArrayList(PersonType.values());
+  }
+
+  @Override
+  public String toString() {
+    return name;
+  }
 }
