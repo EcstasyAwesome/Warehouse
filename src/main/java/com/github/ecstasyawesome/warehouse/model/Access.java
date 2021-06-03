a400575d301705c6802c340c8987777ec35298cc
EcstasyAwesome <ecstasy.awesome@gmail.com>
2021-06-03 13:10:52 +0300

    add static method to check users access

--- src/main/java/com/github/ecstasyawesome/warehouse/model/Access.java
+++ src/main/java/com/github/ecstasyawesome/warehouse/model/Access.java
@@ -1,5 +1,6 @@
 package com.github.ecstasyawesome.warehouse.model;
 
+import com.github.ecstasyawesome.warehouse.model.impl.User;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import javafx.collections.FXCollections;
@@ -20,6 +21,16 @@ public enum Access {
   Access(int level, String name) {
     this.level = level;
     this.name = name;
+  }
+
+  public static boolean isAccessGranted(final User user, Access access) {
+    if (access == GUEST) {
+      return true;
+    }
+    if (user == null) {
+      return false;
+    }
+    return user.getPersonSecurity().getAccess().level <= access.level;
   }
 
   public static ObservableList<Access> getAccesses() {
