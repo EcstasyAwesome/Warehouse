14653b0ebac16c211f39d5fc2247a14672ad805b
EcstasyAwesome <ecstasy.awesome@gmail.com>
2021-06-06 20:25:13 +0300

    use parent type like a parameter

--- src/main/java/com/github/ecstasyawesome/warehouse/controller/AbstractController.java
+++ src/main/java/com/github/ecstasyawesome/warehouse/controller/AbstractController.java
@@ -1,12 +1,12 @@
 package com.github.ecstasyawesome.warehouse.controller;
 
-import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.scene.Parent;
 import javafx.stage.Stage;
 
 public abstract class AbstractController {
 
-  protected final void closeCurrentStage(ActionEvent event) {
+  protected final void closeCurrentStage(Event event) {
     var parent = (Parent) event.getSource();
     var stage = (Stage) parent.getScene().getWindow();
     stage.close();
