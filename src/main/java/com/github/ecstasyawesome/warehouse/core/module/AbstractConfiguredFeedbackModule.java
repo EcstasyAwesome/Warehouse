7385a57c43f5d3dd82d123461ff0b1143b667209
EcstasyAwesome <ecstasy.awesome@gmail.com>
2021-06-07 01:22:11 +0300

    minor changes

--- src/main/java/com/github/ecstasyawesome/warehouse/core/module/AbstractConfiguredFeedbackModule.java
+++ src/main/java/com/github/ecstasyawesome/warehouse/core/module/AbstractConfiguredFeedbackModule.java
@@ -3,7 +3,6 @@ package com.github.ecstasyawesome.warehouse.core.module;
 import com.github.ecstasyawesome.warehouse.core.controller.AbstractConfiguredFeedbackController;
 
 public abstract class AbstractConfiguredFeedbackModule<T extends
-    AbstractConfiguredFeedbackController<E, R>, E, R>
-    extends AbstractConfiguredModule<T, E> {
+    AbstractConfiguredFeedbackController<E, R>, E, R> extends AbstractConfiguredModule<T, E> {
 
 }
