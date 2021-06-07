fbc7dc91d611fd4c986f33d6b257599659358ffb
EcstasyAwesome <ecstasy.awesome@gmail.com>
2021-06-07 14:49:20 +0300

    add converter and validator

--- src/main/java/com/github/ecstasyawesome/warehouse/model/Unit.java
+++ src/main/java/com/github/ecstasyawesome/warehouse/model/Unit.java
@@ -1,29 +1,66 @@
 package com.github.ecstasyawesome.warehouse.model;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.text.ParseException;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.util.StringConverter;
 
 public enum Unit {
 
-  PC, KILO, LITER;
+  PC(0), KILO(3), LITER(3);
 
-  private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat(".00#");
+  private static final StringConverter<Double> CONVERTER = new CustomDoubleStringConverter();
+  private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("0.00#");
+
+  private final int roundingScale;
+
+  Unit(int roundingScale) {
+    this.roundingScale = roundingScale;
+  }
 
   public static ObservableList<Unit> getUnits() {
     return FXCollections.observableArrayList(Unit.values());
   }
 
-  public static String convert(Double value) {
+  public static StringConverter<Double> getConverter() {
+    return CONVERTER;
+  }
+
+  public static String convert(final Double value) {
     return DECIMAL_FORMAT.format(value.doubleValue());
   }
 
-  public static Double convert(String value) {
+  public static Double convert(final String value) {
     try {
       return DECIMAL_FORMAT.parse(value).doubleValue();
     } catch (ParseException e) {
       return 0D;
+    }
+  }
+
+  public Double validate(final Double value) {
+    return new BigDecimal(value.toString())
+        .setScale(roundingScale, RoundingMode.FLOOR)
+        .doubleValue();
+  }
+
+  public static class CustomDoubleStringConverter extends StringConverter<Double> {
+
+    private CustomDoubleStringConverter() {
+    }
+
+    @Override
+    public String toString(Double aDouble) {
+      return Unit.convert(aDouble);
+    }
+
+    @Override
+    public Double fromString(String s) {
+      var value = Unit.convert(s);
+      return value < 0D ? 0D : value;
     }
   }
 }
