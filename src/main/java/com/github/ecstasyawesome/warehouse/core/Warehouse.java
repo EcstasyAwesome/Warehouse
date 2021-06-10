0f99d8fd7fd02deaf60f0ef06205235391303faf
EcstasyAwesome <ecstasy.awesome@gmail.com>
2021-06-10 18:01:57 +0300

    add log info when app was started/closed

--- src/main/java/com/github/ecstasyawesome/warehouse/core/Warehouse.java
+++ src/main/java/com/github/ecstasyawesome/warehouse/core/Warehouse.java
@@ -10,9 +10,18 @@ public class Warehouse extends Application {
   private final Logger logger = LogManager.getLogger(Warehouse.class);
 
   @Override
-  public void start(Stage primaryStage) {
+  public void init() {
     logger.trace("Application started");
+  }
+
+  @Override
+  public void start(Stage primaryStage) {
     WindowManager.initialize(primaryStage);
     WindowManager.getInstance().showAuthorization();
+  }
+
+  @Override
+  public void stop() {
+    logger.trace("Application closed");
   }
 }
