4c5e6303b5240ef8ffb8b195d3665b4999014a8c
EcstasyAwesome <ecstasy.awesome@gmail.com>
2021-06-13 21:57:30 +0300

    add patterns tests

--- src/test/java/com/github/ecstasyawesome/warehouse/util/InputValidatorTest.java
+++ src/test/java/com/github/ecstasyawesome/warehouse/util/InputValidatorTest.java
@@ -1,8 +1,15 @@
 package com.github.ecstasyawesome.warehouse.util;
 
+import static com.github.ecstasyawesome.warehouse.util.InputValidator.EMAIL;
+import static com.github.ecstasyawesome.warehouse.util.InputValidator.LOGIN;
 import static com.github.ecstasyawesome.warehouse.util.InputValidator.NAME;
 import static com.github.ecstasyawesome.warehouse.util.InputValidator.NO_ADJUST;
+import static com.github.ecstasyawesome.warehouse.util.InputValidator.NUMBERS;
+import static com.github.ecstasyawesome.warehouse.util.InputValidator.PASSWORD;
+import static com.github.ecstasyawesome.warehouse.util.InputValidator.PHONE;
 import static com.github.ecstasyawesome.warehouse.util.InputValidator.RED_ADJUST;
+import static com.github.ecstasyawesome.warehouse.util.InputValidator.STRICT_NAME;
+import static com.github.ecstasyawesome.warehouse.util.InputValidator.URL;
 import static com.github.ecstasyawesome.warehouse.util.InputValidator.WILDCARD;
 import static com.github.ecstasyawesome.warehouse.util.InputValidator.arePasswordsEqual;
 import static com.github.ecstasyawesome.warehouse.util.InputValidator.isFieldValid;
@@ -18,6 +25,7 @@ import static org.mockito.Mockito.when;
 
 import com.github.ecstasyawesome.warehouse.core.WindowManager;
 import com.github.ecstasyawesome.warehouse.repository.UniqueField;
+import java.util.stream.IntStream;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -155,5 +163,92 @@ public class InputValidatorTest {
     when(mock.isFieldUnique(anyString())).thenReturn(false);
     assertFalse(isFieldValid("root", "admin", WILDCARD, mock));
     verify(mock, only()).isFieldUnique("root");
+  }
+
+  @Test
+  public void testPhonePattern() {
+    assertFalse("957472288".matches(PHONE.pattern()));
+    assertFalse("0957472288q".matches(PHONE.pattern()));
+    assertFalse("9957472288q".matches(PHONE.pattern()));
+    assertTrue("0957472288".matches(PHONE.pattern()));
+    assertTrue("80957472288".matches(PHONE.pattern()));
+    assertTrue("380957472288".matches(PHONE.pattern()));
+    assertTrue("+380957472288".matches(PHONE.pattern()));
+  }
+
+  @Test
+  public void testStrictNamePattern() {
+    assertTrue("Aa".matches(STRICT_NAME.pattern()));
+    assertTrue("Some".matches(STRICT_NAME.pattern()));
+    assertFalse("S".matches(STRICT_NAME.pattern()));
+    assertFalse("some".matches(STRICT_NAME.pattern()));
+  }
+
+  @Test
+  public void testNamePattern() {
+    assertFalse("S".matches(NAME.pattern()));
+    assertTrue("Aa".matches(NAME.pattern()));
+    assertTrue("Aa wow awesome $$$".matches(NAME.pattern()));
+  }
+
+  @Test
+  public void testLoginPattern() {
+    assertTrue("login".matches(LOGIN.pattern()));
+    assertTrue("login_1568".matches(LOGIN.pattern()));
+    assertFalse("log".matches(LOGIN.pattern()));
+    assertFalse("tooooooooo_long_login".matches(LOGIN.pattern()));
+  }
+
+  @Test
+  public void testPasswordPattern() {
+    assertTrue("07071994".matches(PASSWORD.pattern()));
+    assertTrue("qwertyui".matches(PASSWORD.pattern()));
+    assertTrue("qwertyui123736753".matches(PASSWORD.pattern()));
+    assertFalse("qwerty".matches(PASSWORD.pattern()));
+  }
+
+  @Test
+  public void testNumbersPattern() {
+    IntStream.range(32, 47)
+        .mapToObj(index -> String.valueOf((char) index))
+        .forEach(string -> assertFalse(string.matches(NUMBERS.pattern())));
+
+    IntStream.range(48, 57)
+        .mapToObj(index -> String.valueOf((char) index))
+        .forEach(string -> assertTrue(string.matches(NUMBERS.pattern())));
+
+    IntStream.range(58, 255)
+        .mapToObj(index -> String.valueOf((char) index))
+        .forEach(string -> assertFalse(string.matches(NUMBERS.pattern())));
+  }
+
+  @Test
+  public void testEmailPattern() {
+    assertTrue("some._email@gmail.com".matches(EMAIL.pattern()));
+    assertFalse("some._email@gmail".matches(EMAIL.pattern()));
+    assertFalse("some._email.gmail.com".matches(EMAIL.pattern()));
+    assertFalse("some._@email@gmail.com".matches(EMAIL.pattern()));
+  }
+
+  @Test
+  public void testWildcardPattern() {
+    assertTrue("v".matches(WILDCARD.pattern()));
+    assertTrue("$%&@$".matches(WILDCARD.pattern()));
+    assertTrue(" ".matches(WILDCARD.pattern()));
+    assertTrue("text".matches(WILDCARD.pattern()));
+    assertTrue("some text".matches(WILDCARD.pattern()));
+    assertFalse("".matches(WILDCARD.pattern()));
+  }
+
+  @Test
+  public void testUrlPattern() {
+    assertTrue("example.com".matches(URL.pattern()));
+    assertTrue("example.ua".matches(URL.pattern()));
+    assertTrue("example.ru".matches(URL.pattern()));
+    assertTrue("example.i".matches(URL.pattern()));
+    assertTrue("some_example.com".matches(URL.pattern()));
+    assertTrue("some-example.com".matches(URL.pattern()));
+    assertFalse("example".matches(URL.pattern()));
+    assertFalse("example.".matches(URL.pattern()));
   }
 }
