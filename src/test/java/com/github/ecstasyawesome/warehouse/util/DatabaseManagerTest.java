8f13816c32d381c7438268c446d80039a6fa87d9
EcstasyAwesome <ecstasy.awesome@gmail.com>
2021-06-16 23:02:28 +0300

    fix and simplify test

--- src/test/java/com/github/ecstasyawesome/warehouse/util/DatabaseManagerTest.java
+++ src/test/java/com/github/ecstasyawesome/warehouse/util/DatabaseManagerTest.java
@@ -1,7 +1,6 @@
 package com.github.ecstasyawesome.warehouse.util;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -9,47 +8,24 @@ import com.github.ecstasyawesome.warehouse.ResourceBackupManager;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.StringJoiner;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class DatabaseManagerTest {
 
-  private static final Path DB_PATH = Path.of("database");
-  private static final ResourceBackupManager BACKUP_MANAGER = new ResourceBackupManager(DB_PATH);
-
-  @BeforeEach
-  public void setUp() throws IOException {
-    BACKUP_MANAGER.backup();
-  }
-
-  @AfterEach
-  public void tearDown() throws IOException {
-    BACKUP_MANAGER.restore();
-  }
-
   @Test
-  public void databaseCreatedAndConnectionAvailable() {
-    assertTrue(Files.notExists(DB_PATH));
-    try (var connection = DatabaseManager.getConnection()) {
-      assertNotNull(connection);
-    } catch (SQLException exception) {
-      fail(exception);
-    }
-    assertTrue(Files.exists(DB_PATH.resolve("default.mv.db")));
-  }
-
-  @Test
-  public void tablesSuccessfullyChecked() {
-    assertTrue(Files.notExists(DB_PATH));
+  public void databaseCreated() throws IOException {
+    var databasePath = Path.of("database");
+    var resourceManager = new ResourceBackupManager(databasePath);
+    resourceManager.backup();
+    assertTrue(Files.notExists(databasePath));
     var expected = new String[]{"CATEGORIES", "COMPANIES", "COMPANIES_ADDRESSES", "ORDERS_ITEMS",
         "COMPANIES_CONTACTS", "PRODUCTS", "PRODUCT_PROVIDERS", "PRODUCT_PROVIDERS_ADDRESSES",
         "PRODUCT_PROVIDERS_CONTACTS", "PRODUCT_STORAGES", "PRODUCT_STORAGES_ADDRESSES", "ORDERS",
         "PRODUCT_STORAGES_CONTACTS", "USERS", "USERS_CONTACTS", "USERS_SECURITY"};
     try (var connection = DatabaseManager.getConnection()) {
+      DatabaseManager.createTablesIfAbsent(connection);
       var metaData = connection.getMetaData();
       try (var resultSet = metaData.getTables(null, null, null, new String[]{"TABLE"})) {
         var joiner = new StringJoiner(";");
@@ -64,5 +40,7 @@ public class DatabaseManagerTest {
     } catch (Throwable throwable) {
       fail(throwable);
     }
+    assertTrue(Files.exists(databasePath.resolve("default.mv.db")));
+    resourceManager.restore();
   }
 }
